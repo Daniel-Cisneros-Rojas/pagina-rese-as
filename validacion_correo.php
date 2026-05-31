5d92c3d039d654d118e46126a9afd8d7437bc4f3
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-05-31 12:52:15 -0600

    Update validacion_correo.php
    Removed hardcoded SMTP credentials for security.

--- validacion_correo.php
+++ validacion_correo.php
@@ -41,8 +41,8 @@ $mail = new PHPMailer(true);
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'dan493075@gmail.com';                     //SMTP username
-            $mail->Password   = 'lsnc ddrr xiuq ymko';                               //SMTP password
+            $mail->Username   = '';                     //SMTP username
+            $mail->Password   = '';                               //SMTP password
             $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
